097d803d02eed005319fa73141218630f2f08526
Lucas Okazaki <l.h.okazaki@gmail.com>
2025-09-21 20:07:04 -0300

    feat: new form "delivery data"

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import DadosUsuario from "./DadosUsuario";
 import DadosPessoais from "./DadosPessoais";
+import DadosEntrega from "./DadosEntrega";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
     return (
         <>
             <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
             <DadosUsuario />
+            <DadosEntrega/>
         </>
     );
 }
